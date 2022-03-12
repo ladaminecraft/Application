a7bf533dd1f038cdb914f0875fa9280ad9912b7a
Lada <ladaminecraft@mail.ru>
2022-03-12 16:54:16 +0300

    Добавила первые циклы и условия - вторая

--- script.js
+++ script.js
@@ -8,12 +8,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt ('Последний посмотренный фильм?', ''),
-      b = prompt ('Оценка', ''),
-      c = prompt ('Последний посмотренный фильм?', ''),
-      d = prompt ('Оценка', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log (personalMovieDB);
